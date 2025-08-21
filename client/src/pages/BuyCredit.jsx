8eb5467becb067243f5d5ff2b290883f3e90ece0
gyan011 <gyanranjan9661@gmail.com>
2025-08-21 16:56:15 +0530

    changes

--- client/src/pages/BuyCredit.jsx
+++ client/src/pages/BuyCredit.jsx
@@ -3,7 +3,10 @@ import React from 'react'
 const BuyCredit = () => {
   return (
     <div>
-      BuyCredit
+      <button className='min- '>
+        Our Plans
+      </button>
+      <h1>Choose the plan</h1>
     </div>
   )
 }
