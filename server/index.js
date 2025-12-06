a61e114275db143fe1fafeb54a681f09d4c0f635
gyan011 <gyanranjan9661@gmail.com>
2025-12-06 15:15:21 +0530

    final

--- server/index.js
+++ server/index.js
@@ -10,7 +10,7 @@ const app = express();
 app.use(express.json());
 dotenv.config();
 app.use(cors({
-  origin: "http://localhost:5173",
+  origin: process.env.FRONTEND_URL,
   credentials: true,
   allowedHeaders: ["Content-Type", "Authorization", "token"], 
 }));
